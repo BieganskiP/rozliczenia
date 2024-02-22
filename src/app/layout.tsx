d76fbe37bce9a6a5f4a4e90b5202036ae1995906
BieganskiP <bieganski1996@gmail.com>
2024-02-22 16:28:57 +0100

    delte unused code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import { Inter as FontSans } from "next/font/google";
-import { initThinBackend } from "thin-backend";
-import { ThinBackend } from "thin-backend-react";
 
 import { cn } from "@/lib/utils";
 import "../styles/globals.css";
 
 import Nav from "@/components/Nav";
-
-initThinBackend({ host: process.env.NEXT_PUBLIC_BACKEND_URL });
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -22,19 +18,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ThinBackend>
-      <html lang="en">
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
-        >
-          <Nav />
-          {children}
-        </body>
-      </html>
-    </ThinBackend>
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <Nav />
+        {children}
+      </body>
+    </html>
   );
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 "use client";
-import UserStatus from "@/api/UserStatus";
 
 export default function Home() {
-  return (
-    <main>
-      <UserStatus />
-    </main>
-  );
+  return <main></main>;
 }
